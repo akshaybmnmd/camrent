5cb251e7e9930e5fa5c6f0b04f49870501bc2a22
Akshay BM <akshaybmnmd@gmail.com>
2021-04-24 12:16:00 +0530

    option for user to update

--- index.php
+++ index.php
@@ -46,53 +46,73 @@ $conn->close();
 </head>
 
 <body>
-    <form name="myForm" action="./action_page.php" method="POST" enctype='multipart/form-data'>
-        <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" class="form-control" name="name" required size="30">
+    <div id="bookcammodel" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 id='model3Heading' class="modal-title">Modal 3 Header</h4>
+                </div>
+                <div class="modal-body">
+                    <form name="myForm" action="./action_page.php" method="POST" enctype='multipart/form-data'>
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" class="form-control" name="name" required size="30">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone Number:</label>
+                            <input type="tel" class="form-control" name="phone" pattern="[0-9]{10}" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" class="form-control" name="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Address:</label>
+                            <textarea class="form-control" name="address" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="bookeddate">Date:</label>
+                            <input type="text" id="date" class="form-control" name="bookeddate" readonly required />
+                        </div>
+                        <div class="form-group">
+                            <label for="duration">Duration:</label>
+                            <input type="number" id='durtn' class="form-control" name="duration" max='9' min='1' value="1" pattern="[0-9]{1}" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="date">Camera:</label>
+                            <select class="form-control" name="camera" value='250' readonly required>
+                                <option value="250" id='cam' selected='true'>Canon 1500D</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="kitlens"> Kit lens (18-50mm)</label>
+                            <input type="checkbox" id='a' name="kitlens" value="100" checked><br>
+                            <label for="zoomlens">Zoom lens (55 - 250mm)</label>
+                            <input type="checkbox" id='b' name="zoomlens" value="150"><br>
+                            <label for="primelens"> Prime lens (50mm)</label>
+                            <input type="checkbox" id='c' name="primelens" value="150">
+                        </div>
+                        <div class="form-group">
+                            <label for="myfile">Upload a ID proof:</label>
+                            <input type="file" class="form-control" name="file[]" id="file" multiple>
+                            <!-- <input type="file" id="myfile" class="form-control" name="myfile"> -->
+                        </div>
+                        <input type="submit" class="btn btn-default" value="Submit">
+                    </form>
+                    <div id='amound'>Call Me at +91 8606815571</div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal" onClick="updaterent()">Submit</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
         </div>
-        <div class="form-group">
-            <label for="phone">Phone Number:</label>
-            <input type="tel" class="form-control" name="phone" pattern="[0-9]{10}" required>
-        </div>
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" class="form-control" name="email">
-        </div>
-        <div class="form-group">
-            <label for="address">Address:</label>
-            <textarea class="form-control" name="address" required></textarea>
-        </div>
-        <div class="form-group">
-            <label for="bookeddate">Date:</label>
-            <input type="text" id="date" class="form-control" name="bookeddate" readonly required />
-        </div>
-        <div class="form-group">
-            <label for="duration">Duration:</label>
-            <input type="number" id='durtn' class="form-control" name="duration" max='9' min='1' value="1" pattern="[0-9]{1}" required />
-        </div>
-        <div class="form-group">
-            <label for="date">Camera:</label>
-            <select class="form-control" name="camera" value='250' readonly required>
-                <option value="250" id='cam' selected='true'>Canon 1500D</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="kitlens"> Kit lens (18-50mm)</label>
-            <input type="checkbox" id='a' name="kitlens" value="100" checked><br>
-            <label for="zoomlens">Zoom lens (55 - 250mm)</label>
-            <input type="checkbox" id='b' name="zoomlens" value="150"><br>
-            <label for="primelens"> Prime lens (50mm)</label>
-            <input type="checkbox" id='c' name="primelens" value="150">
-        </div>
-        <div class="form-group">
-            <label for="myfile">Upload a ID proof:</label>
-            <input type="file" class="form-control" name="file[]" id="file" multiple>
-            <!-- <input type="file" id="myfile" class="form-control" name="myfile"> -->
-        </div>
-        <input type="submit" class="btn btn-default" value="Submit">
-    </form>
-    <div id='amound'>Call Me at +91 8943705571</div>
+    </div>
+    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#bookcammodel" onClick=editrent(' + row[0] + ')>Update rent</button>
     <script type="text/javascript">
         var unavailableDates = [<?php echo $dates; ?>];
 
@@ -109,13 +129,7 @@ $conn->close();
             }
         }
         $(function() {
-            $('#date').datepicker({
-                beforeShowDay: available,
-                autoSize: true,
-                dateFormat: "dd-m-yy"
-            });
-        })
-    </script>
+            $(' #date').datepicker({ beforeShowDay: available, autoSize: true, dateFormat: "dd-m-yy" }); }) </script>
 </body>
 
 </html>
